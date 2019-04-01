fbcf00fc8c8e0b6a07b81e9118177a1f5545f5a7
余倩 <qianyu@harmonycloud.cn>
2019-04-01 10:46:55 +0800

    war包部署

--- src/main/java/com/harmonycloud/sagaquery/SagaSearchApplication.java
+++ src/main/java/com/harmonycloud/sagaquery/SagaSearchApplication.java
@@ -4,19 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = {
-		SecurityAutoConfiguration.class,
-		
+        SecurityAutoConfiguration.class,
 })
 
 @EnableJpaRepositories
 @EntityScan("com.harmonycloud.sagaquery.entity")
-public class SagaSearchApplication{
+public class SagaSearchApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SagaSearchApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SagaSearchApplication.class, args);
+    }
 
 }
